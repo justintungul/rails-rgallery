42675bc3b706d0bf02e231171bfb11df21a5a6f7
Justin Tungul <justin.tungul@gmail.com>
2018-06-20 15:41:13 -0700

    Add edit and delete functinos

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,9 +14,21 @@ class CategoriesController < ApplicationController
   end
 
   def edit
+    @category = Category.find(params[:id])
+    @categories = Category.all
+    @images = @category.images
   end
 
   def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      flash[:notice] = "Category Updated"
+
+      redirect_to category_path(params[:id])
+    else
+      render 'edit'
+    end
   end
 
   def destroy
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,16 +1,27 @@
 class ImagesController < ApplicationController
   def new
-    @image = Image.new
+  	@image = Image.new
   end
 
   def create
     @image = Image.new(image_params)
+
     if @image.save
       flash[:notice] = "Image Created"
+
       redirect_to root_path
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+
+    flash[:notice] = "Image Removed"
+
+    redirect_to images_path
   end
 
   def index
@@ -18,12 +29,8 @@ class ImagesController < ApplicationController
     @categories = Category.all
   end
 
-  def show
-  end
-
   private
-
-  def image_params
-    params.require(:image).permit(:image, :category_id, :image_title, :image_description, :image_file_size, :image_content_type, :remote_image_url)
-  end
+    def image_params
+      params.require(:image).permit(:image, :category_id, :image_title, :image_description, :image_file_size, :image_content_type, :remote_image_url)
+    end
 end
